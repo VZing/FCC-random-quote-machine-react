f2525dcf5f5b786cb1bba0299d22d17dad0dde98
veronica <veronica.zingarelli@gmail.com>
2019-10-21 15:19:25 -0500

    made a .env file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Random Quote Generator
-          <div id="quote-box">
-            <div id= "text"></div>
-            <div id= "author"></div>
-            <button id= "new-quote">New Quote</button>
-           
+         <div id="quote-box">
+           <div id= "quote">
+            <div id= "text">"Art is what you can get away with"</div>
+            <div id= "author">- Andy Warhol</div>
+            </div>
           </div>
+          <button id= "new-quote">New Quote</button>
+          {/* ReactDOM.render( 
+    quote, 
+    document.getElementById("root")); */}
+          {/*should render new text & corresponding author elements when button is clicked ReactDom.render(text, ) */}
         </p>
         <a
           className="App-link"
