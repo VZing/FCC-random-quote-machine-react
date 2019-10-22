ceabaa150f342376b80a03e5709cba5d4c223947
veronica <veronica.zingarelli@gmail.com>
2019-10-22 12:17:31 -0500

    app.js is stateful

--- src/App.js
+++ src/App.js
@@ -1,44 +1,80 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
+import Quote from './Quote.js';
 import './App.css';
 
-const API_KEY =`${process.env.REACT_APP_QUOTE_API_KEY}`
-console.log(API_KEY)
+// const API_KEY =`${process.env.REACT_APP_QUOTE_API_KEY}`
+// console.log(API_KEY)
 
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
-         <h2> Random Quote Generator</h2>
-         <div id="quote-box">
-           <div id= "quote">
-            <div id= "text">"Art is what you can get away with"</div>
-            <div id= "author">- Andy Warhol</div>
-            {/* <div id= "text">{API_KEY.quoteText}</div>
-            <div id= "author">- {API_KEY.quoteAuthor}</div> */}
-          <button id= "new-quote">New Quote</button>
+  constructor(props){
+    super(props);
+    // this.state = {quote: new Quote()}
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+           <h2> Random Quote Generator</h2>
+           <div id="quote-box">
+             <div id= "quote">
+              <div id= "text">"Art is what you can get away with"</div>
+              <div id= "author">- Andy Warhol</div>
+          
+            <button id= "new-quote">New Quote</button>
+              </div>
             </div>
-          </div>
-          {/* ReactDOM.render( 
-    quote, 
-    document.getElementById("root")); */}
-          {/*should render new text & corresponding author elements when button is clicked ReactDom.render(text, ) */}
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-     
-    </div>
-  );
+  
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+       
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+
+//          <h2> Random Quote Generator</h2>
+//          <div id="quote-box">
+//            <div id= "quote">
+//             <div id= "text">"Art is what you can get away with"</div>
+//             <div id= "author">- Andy Warhol</div>
+        
+//           <button id= "new-quote">New Quote</button>
+//             </div>
+//           </div>
+//           {/* ReactDOM.render( 
+//     quote, 
+//     document.getElementById("root")); */}
+//           {/*should render new text & corresponding author elements when button is clicked ReactDom.render(text, ) */}
+
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+     
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Quote.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// import './App.css';
+// const API_KEY =`${process.env.REACT_APP_QUOTE_API_KEY}`
+// console.log(API_KEY)
+// export default Quote;
+
