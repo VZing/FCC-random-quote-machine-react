108cdb2da518a8590f22f21dbfb59e2c8919f7da
veronica <veronica.zingarelli@gmail.com>
2019-10-23 17:06:15 -0500

    thats all

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 
@@ -7,8 +8,9 @@ const Tweet = (props) => {
         // <button id="tweet-quote" onClick={props.getQuote}>
         //     Tweet This
         //       </button>
-        <a id="tweet-quote" href= 'twitter.com/intent/tweet' > Tweet This </a>
+        <a id="tweet-quote" href='twitter.com/intent/tweet' > Tweet This </a>
     );
 }
-
+// href='twitter.com/intent/tweet'
+// href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote})}
 export default Tweet
