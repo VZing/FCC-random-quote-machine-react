d6ebfaa0718d716687615f1652773f62a0e0540f
veronica <veronica.zingarelli@gmail.com>
2019-10-23 12:05:27 -0500

    xxxxxxx

--- src/Quote.js
+++ src/Quote.js
@@ -8,49 +8,9 @@ export default class Quote extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
+           isLoading: true,
+           quotes: [],
+           error: null
         };
     }
-
-    
-    // function getQuote(){
-    //     $.ajax({
-    //         url: API_KEY,
-    //         type: 'GET',
-    //         datatype: 'json',
-    //         success: function(data){
-    //             alert(JSON.stringify(data))
-    //         },
-    //         error: function(err){ 
-    //         }
-    //     })
-    // }
-    componentDidMount(){
-
-        fetch(API_KEY)
-        .then(quoteInfo => {
-            console.log(quoteInfo.json())
-
-        
-            
-        })
-    
-    }
-
-    render() {
-        // getQuote();
-      return (
-        <div id="quote-box">
-             <div id= "quote">
-              <div id= "text">quote</div>
-              <div id= "author">- author</div>
-              <Button />
-              </div>
-            </div>
-      );
-    }
-  }
-
 
