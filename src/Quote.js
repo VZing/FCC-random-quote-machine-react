56cd44974bf9d5425ca85e9c03f0d5124ce3efed
veronica <veronica.zingarelli@gmail.com>
2019-10-23 12:28:54 -0500

    getQuote is bound

--- src/Quote.js
+++ src/Quote.js
@@ -1,16 +1,43 @@
+
 import React from 'react';
-// import $ from 'jquery';
+import axios from 'axios';
 import Button from './Button.js';
 
-const API_KEY =`${process.env.REACT_APP_QUOTE_API_KEY}`
+const API_KEY = `${process.env.REACT_APP_QUOTE_API_KEY}`
 console.log(API_KEY);
 export default class Quote extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-           isLoading: true,
-           quotes: [],
-           error: null
+            error: null,
+            isLoaded: false,
+            items: []
         };
+        this.getQuote = this.getQuote.bind(this);
     }
+
+
+    getQuote(){
+        axios.get(API_KEY)
+            .then(({ data }) => {
+                console.log(data.quoteText);
+            });
+    }
+    componentDidMount() {
+        this.getQuote();
+    }
+
+    render() {
+  
+        return (
+            <div id="quote-box">
+                <div id="quote">
+                    <div id="text">quote</div>
+                    <div id="author">- author</div>
+                    <Button />
+                </div>
+            </div>
+        );
+    }
+}
 
